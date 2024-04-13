0e749c56565abd190133829d12f1ff5a46f7123f
SimonAMoore <166959850+SimonAMoore@users.noreply.github.com>
2024-04-14 00:35:33 +0100

    Add files via upload

---
+++ SST39SF0x0-Serial.ino
@@ -0,0 +1,161 @@
+const static String   PROGRAM_TITLE     = "Flash EPROM Programmer";
+const static String   PROGRAM_COPYRIGHT = "(C) Simon Moore. All Rights Reserved.";
+const static String   PROGRAM_VERSION   = "Version 1.0.2";
+const static String   PROGRAM_DATE      = "12th April 2024.";
+
+const static uint16_t STR_BUFFER_SIZE   = 80;       // Size of temporary buffer for sprintf()
+const static uint32_t BAUD_RATE         = 921600;   // Serial port BAUD rate
+const static uint32_t SERIAL_TIMEOUT    = 10000;    // Command timeout length in milliseconds
+const static uint8_t  COMMAND_NUM       = 14;       // Number of commands in command list array
+
+// List of serial protocol commands
+// ================================
+//
+// Send handshake character 'z'
+// Receive acknowledge character 'Z'
+//
+// CMD_ID char   Function            Bytes  Input/Output         Description
+// ==================================================================================================================================
+// SDBLSZ [b|B]  Set data block size  (4)   -> (WORD <= 0x1000)     Set size of data block for binary data transfers
+// GDEVID [d|D]  Get device id        (0)   <- (WORD:WORD)          Get the manufacturer and device ID for EPROM
+// SSADDR [s|S]  Set start address    (5)   -> (DWORD < 0x6ffff)    Set the start address for future commands
+// RDBYTE [r]    Read data byte       (0)   <- (BYTE)               Read EPROM byte from current address
+// RDBLCK [R]    Read data block      (0)   <- (Data Block)         Read EPROM block from current address
+// WDBLCK [w|W]  Write data block     (D)   -> (Data Block)         Write block of data starting at current address
+// EEBANK [e]    Erase EPROM bank     (4)   -> (DWORD)              Erase bank of EPROM at address (bank masked) if data = 0xaa55aa55
+// EEPROM [E]    Erase EPROM         (16)   -> (String[16])         Erase entire EPROM if data matches CRC32 hash value
+// GCRC32 [g]    Get EPROM CRC        (0)   <- (String[16])         Get EPROM's CRC32 hash value
+// GSHA-1 [G]    Get EPROM SHA-1      (0)   <- (String[40])         Get EPROM's SHA-1 hash value
+// ==================================================================================================================================
+
+// Command structure
+struct Command {
+  char    c;    // command character code
+  int8_t  d;    // Size of data in bytes (-1 for binary data block)
+  String  str;  // Command ID string
+};
+
+// List of serial protocol commands
+Command commandList[COMMAND_NUM] = { { 'b',  4, "SDBLSZ"}, { 'B',  4, "SDBLSZ"},
+                                     { 'd',  0, "GDEVID"}, { 'D',  0, "GDEVID"},
+                                     { 's',  5, "SSADDR"}, { 'S',  5, "SSADDR"},
+                                     { 'r',  0, "RDBYTE"}, { 'R',  0, "RDBLCK"},
+                                     { 'w', -1, "WDBLCK"}, { 'W', -1, "WDBLCK"},
+                                     { 'e',  4, "EEBANK"}, { 'E', 16, "EEPROM"},
+                                     { 'g',  0, "GCRC32"}, { 'G',  0, "GSHA-1"}
+                                    };
+
+int8_t cmdB = 0;    // Current command index into command list
+
+// State machine program states
+enum PState { ERROR,
+              IDLE,
+              WAIT_CMD_B,
+              WAIT_CMD_D,
+              RECV_DBLOCK,
+              CMD_RUN };
+
+PState pState = IDLE;   // Current program state
+
+uint32_t lastMillis = millis();   // 32 bit counter for timeout
+
+char strBuffer[STR_BUFFER_SIZE] = "";   // Buffer for sprintf() text function
+
+void setup() {
+  Serial.begin(BAUD_RATE, SERIAL_8N1);
+  Serial.println();
+  Serial.println(PROGRAM_TITLE);
+  Serial.println(PROGRAM_COPYRIGHT);
+  Serial.println(PROGRAM_VERSION);
+  Serial.println(PROGRAM_DATE);
+  Serial.println();
+}
+
+void loop() {
+  switch(pState) {
+    // Device error state.
+    default:
+    case ERROR:
+      HALT("HALTED: Device Error State");
+      break;
+
+    // Waiting for handshake character 'z'
+    case IDLE:
+      if (Serial.available() > 0) {
+        if (Serial.read() == 'z') {
+          Serial.write('Z');          // Confirm handshake with 'Z'
+          pState = WAIT_CMD_B;        // Set program state to WAIT_CMD_B (wait for command byte)
+          cmdB = 0;                   // Reset current command
+        }
+      }
+      lastMillis = millis();          // Reset timeout counter
+      break;
+
+    // Waiting for command byte code
+    case WAIT_CMD_B:
+      if (Serial.available() > 0) {
+        // Get command character from serial data
+        char c = Serial.read();
+
+        // Set program state to idle in case no valid command parsed
+        pState = IDLE;
+        cmdB = 0;
+
+        // Parse command character
+        for (uint8_t i = 0; i < COMMAND_NUM; i++) {
+          if (c == commandList[i].c) {
+            cmdB = i;
+            // Set next program state based on command's data byte length
+            switch (commandList[i].d) {
+              default:
+                pState = WAIT_CMD_D;
+                break;
+              
+              case -1:
+                pState = RECV_DBLOCK;
+                break;
+
+              case 0:
+                pState = CMD_RUN;
+                break;
+            }
+            break;
+          }
+        }
+      }
+      break;
+
+    // Waiting for command data
+    case WAIT_CMD_D:
+      Serial.println("WAIT_CMD_D");
+      Serial.println(commandList[cmdB].str);
+      break;
+
+    // Receive binary data block
+    case RECV_DBLOCK:
+      Serial.println("RECV_DBLOCK");
+      Serial.println(commandList[cmdB].str);
+      break;
+
+    // Execute command function
+    case CMD_RUN:
+      Serial.println("CMD_RUN");
+      Serial.println(commandList[cmdB].str);
+      break;
+  }
+
+  // If timer exceeds timeout value, reset program state to idle
+  if (millis() - lastMillis > SERIAL_TIMEOUT) {
+    Serial.println("IDLE");
+    pState = IDLE;
+  }
+
+  // Delay for debuging
+  delay(1000);
+}
+
+void HALT(String msg) {
+  Serial.println(msg);
+  Serial.flush();
+  Serial.end();
+}
